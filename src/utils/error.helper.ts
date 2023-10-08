625c1cb526af1b2f197fa5d9a6ec3cf9b9dd4689
helewud <helewud@gmail.com>
2023-10-08 09:06:44 +0100

    chore: add class validator error type

--- src/utils/error.helper.ts
+++ src/utils/error.helper.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { ValidationError } from "class-validator";
 
-// Custom error class to distinguish application errors from standard errors
 class AppError extends Error {
   public readonly statusCode: number;
   public readonly isOperational: boolean;
 
   constructor(message: string, statusCode: keyof typeof StatusCodes) {
+    // Check if the message starts with "Error:" and remove it if present
+
     super(message);
     this.statusCode =
       StatusCodes[statusCode] || StatusCodes.INTERNAL_SERVER_ERROR;
@@ -15,31 +17,58 @@ class AppError extends Error {
   }
 }
 
-// Middleware to handle application errors
+function transformClassValidatorErrors(errors: ValidationError[]) {
+  const messages: string[] = [];
+
+  for (const error of errors) {
+    if (error.constraints) {
+      messages.push(...Object.values(error.constraints));
+    }
+
+    if (error.children && error.children.length > 0) {
+      const childMessages = transformClassValidatorErrors(error.children);
+      messages.push(...childMessages);
+    }
+  }
+
+  return messages;
+}
+
 const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  // Default error status code and message
-  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
-  let message: string = ReasonPhrases.INTERNAL_SERVER_ERROR;
+  if (res.headersSent) {
+    console.error(err);
+    return;
+  }
 
-  // Handle known application errors
+  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+  let message: any = ReasonPhrases.INTERNAL_SERVER_ERROR;
+
   if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   }
 
-  // Log the error
+  if (Array.isArray(err) && err[0] instanceof ValidationError) {
+    statusCode = StatusCodes.BAD_REQUEST;
+    message = transformClassValidatorErrors(err as ValidationError[]);
+  }
+
   console.error(err);
 
-  // Send an error response to the client
+  const formattedMessage =
+    typeof message === "string" && message.startsWith("Error:")
+      ? message.substring("Error:".length).trim()
+      : message;
+
   res.status(statusCode).json({
     status: "error",
     statusCode,
-    message,
+    message: formattedMessage,
   });
 };
 
